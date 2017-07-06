fbc8f77b996a02db1362a4e9f8027e00d17ab1eb
aFriendlySword <afriendlysword@gmail.com>
2017-07-06 11:14:42 +0200

    connected

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var sys = require("sys"),
 var http = require('http');
 var server = http.createServer(function (request, response) { });
 
-server.listen(1234, function () {
+server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 1234');
 });
 
@@ -21,8 +21,27 @@ wsServer.on('request', function (r) {
     var id = count++;
     clients[id] = connection;
 
+    clients[id].send(JSON.stringify({
+        type: CONNECTED,
+    }));
+
     connection.on('message', function (message)) {
-    
+        var msgString = JSON.parse(message.data);
+
+        switch (message.type) {
+            case JOIN:
+                clients[id].name = message.name;
+                clients[id].color = message.color;
+                clients[id].x = 350;
+                clients[id].y = 300;                
+
+            case MOUSE_COORDS:
+                clients[id].angle = message.rot;
+                break;
+            case MOVE_START:
+
+        }
+
     }
 
 
